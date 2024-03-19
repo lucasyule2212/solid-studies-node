c853ff3445bad521dff3d00095dc0196f475b09a
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-19 09:38:45 -0300

    feat(app.ts): adding first route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './services/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  // ? 201 - because this is a CREATE operation that does not return any data
+  return reply.status(201).send()
+})
