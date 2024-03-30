36913f1eb5a46945c0ec5b6be98e27f7cbe6e7db
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-30 12:44:41 -0300

    feat(utils): adding utility consts and function to checkin use-case

---
+++ src/utils/consts.ts
@@ -0,0 +1,16 @@
+const userTestLatitude = -8.0302454
+const userTestLongitude = -34.9210607
+
+const furtherGymLatitude = -8.0361897
+const furtherGymLongitude = -34.9067559
+
+// Maximum distance between the user and the gym
+const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
+export {
+  MAX_DISTANCE_IN_KILOMETERS,
+  furtherGymLatitude,
+  furtherGymLongitude,
+  userTestLatitude,
+  userTestLongitude,
+}
