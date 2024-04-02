de8bee95697bef2f99a7e57a0ef345d1464c4d8d
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-02 11:20:28 -0300

    refactor(checkins-repository): implementing remaining methods

--- src/repositories/prisma-checkins-repository.ts
+++ src/repositories/prisma-checkins-repository.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/services/prisma'
-import { Prisma, PrismaClient } from '@prisma/client'
+import { CheckIn, Prisma, PrismaClient } from '@prisma/client'
+import dayjs from 'dayjs'
 import { ICheckinsRepository } from './interfaces/checkins-repository'
 
 export class PrismaCheckinsRepository implements ICheckinsRepository {
@@ -7,6 +8,51 @@ export class PrismaCheckinsRepository implements ICheckinsRepository {
 
   constructor() {
     this.prismaClient = prisma
+  }
+
+  async findManyByUserId({ user_id, page }: { user_id: string; page: number }) {
+    const checkIns = await this.prismaClient.checkIn.findMany({
+      where: {
+        user_id,
+      },
+      skip: page * 10,
+      take: 10,
+      orderBy: {
+        created_at: 'desc',
+      },
+    })
+    return checkIns
+  }
+
+  async countByUserId({ user_id }: { user_id: string }) {
+    const count = await this.prismaClient.checkIn.count({
+      where: {
+        user_id,
+      },
+    })
+    return count
+  }
+
+  async findById({ id }: { id: string }) {
+    const checkIn = await this.prismaClient.checkIn.findUnique({
+      where: {
+        id,
+      },
+    })
+    return checkIn
+  }
+
+  async save(checkIn: CheckIn) {
+    const savedCheckIn = await this.prismaClient.checkIn.update({
+      where: {
+        id: checkIn.id,
+      },
+      data: {
+        validated_at: checkIn.validated_at,
+      },
+    })
+
+    return savedCheckIn
   }
 
   async create({ gym_id, user_id }: Prisma.CheckInUncheckedCreateInput) {
@@ -21,16 +67,19 @@ export class PrismaCheckinsRepository implements ICheckinsRepository {
   }
 
   async findByUserIdOnDate({ user_id, date }: { user_id: string; date: Date }) {
+    const startOfDay = dayjs(date).startOf('date')
+    const endOfDay = dayjs(date).endOf('date')
+
     const checkInOnSameDate = await this.prismaClient.checkIn.findFirst({
       where: {
         user_id,
         created_at: {
-          gte: date,
-          lt: new Date(date.getTime() + 24 * 60 * 60 * 1000),
+          gte: startOfDay.toDate(),
+          lte: endOfDay.toDate(),
         },
       },
     })
 
-    return checkInOnSameDate || null
+    return checkInOnSameDate
   }
 }
