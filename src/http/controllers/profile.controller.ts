280573128d9246a8458ba2684da9f0d98ece826f
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-02 13:32:48 -0300

    feat(profile.controller): implementing profile.controller

---
+++ src/http/controllers/profile.controller.ts
@@ -0,0 +1,14 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const getUserProfile = makeGetUserProfileUseCase()
+    const { user } = await getUserProfile.execute({ id: request.user.sub })
+
+    return reply.status(200).send(user)
+  } catch (error) {
+    // ? 500 - because this is an unexpected error
+    return reply.status(500).send()
+  }
+}
