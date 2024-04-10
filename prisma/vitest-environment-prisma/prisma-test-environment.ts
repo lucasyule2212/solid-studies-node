5f8a2fc4394aeeb83c552db241a9a7303d2f6f18
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-09 23:49:47 -0300

    feat(vitest-env-prisma): adding generateDatabaseUrl method + new db url

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,9 +1,34 @@
+import { env } from '@/env'
+import { execSync } from 'child_process'
+import { randomUUID } from 'crypto'
 import { Environment } from 'vitest'
+
+function generateDatabaseUrl(schema: string) {
+  if (!env.DATABASE_URL) {
+    throw new Error(
+      'Please provide a DATABASE_URL in the environment variables',
+    )
+  }
+
+  const url = new URL(env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
+    const schema = randomUUID()
+
+    const databaseURL = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy --preview-feature')
+
     return {
       teardown() {},
     }
