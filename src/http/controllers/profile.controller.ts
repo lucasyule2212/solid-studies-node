7b09374eb5c76c781442492c0c4cea173922e3e9
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-10 23:08:49 -0300

    fix(profile.controller): fixing profile route return

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -6,7 +6,7 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
     const getUserProfile = makeGetUserProfileUseCase()
     const { user } = await getUserProfile.execute({ id: request.user.sub })
 
-    return reply.status(200).send(user)
+    return reply.status(200).send({ user })
   } catch (error) {
     // ? 500 - because this is an unexpected error
     return reply.status(500).send()
