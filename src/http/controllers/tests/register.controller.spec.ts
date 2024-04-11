9876fa7003df11b90851cdb59ab00e0bdb2ee82e
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-10 22:47:00 -0300

    feat(controllers-tests-register): adding first e2e register test

--- src/http/controllers/tests/register.controller.spec.ts
+++ src/http/controllers/tests/register.controller.spec.ts
@@ -1,3 +1,25 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('register', async () => {})
+describe('Register Controller (e2e)', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('Should be able to register', async () => {
+    // ? "request" from "supertest" is a function that mocks the HTTP requests
+    // ? By doing so, we can test the HTTP routes without having to run the server
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'zxczxc',
+    })
+
+    expect(response.status).toBe(201)
+  })
+})
