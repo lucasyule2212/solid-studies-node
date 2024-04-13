4e3471ff4bb9e22539c4ed7976de521a9ee6077d
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-12 23:01:09 -0300

    feat(checkins-routes): setting checkins routes

---
+++ src/http/controllers/checkins/route.ts
@@ -0,0 +1,16 @@
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { FastifyInstance } from 'fastify'
+import { create } from './create.controller'
+import { history } from './history.controller'
+import { metrics } from './metrics.controller'
+import { validate } from './validate.controller'
+
+export async function gymsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+
+  app.get('/checkins/history', history)
+  app.get('/checkins/metrics', metrics)
+
+  app.post('/gyms/:gymId/checkins', create)
+  app.patch('/checkins/:checkinId/validate', validate)
+}
