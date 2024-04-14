b9a8029ed4ff173fa7557feda2f18acda45a28f4
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-13 23:56:38 -0300

    feat(create-gym-test): adding base create-gym test

---
+++ src/http/controllers/gyms/tests/create.controller.spec.ts
@@ -0,0 +1,32 @@
+import { app } from '@/app'
+import { userTestLatitude, userTestLongitude } from '@/utils/consts'
+import { createAndAuthUser } from '@/utils/test/create-and-auth-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Gym Controller (e2e)', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('Should be able to create a gym', async () => {
+    const { token } = await createAndAuthUser(app)
+
+    const profileResponse = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Gym Title',
+        description: 'Gym Description',
+        phone: '123456789',
+        latitude: userTestLatitude,
+        longitude: userTestLongitude,
+      })
+
+    expect(profileResponse.status).toBe(201)
+  })
+})
