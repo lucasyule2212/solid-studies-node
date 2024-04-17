07d5c2e8387e4cad00b6d8b50d9e87ecd360b129
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-16 23:26:49 -0300

    feat(app-server): adding fastify-cookie plugin setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import fastifyCookie from '@fastify/cookie'
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
@@ -11,6 +12,7 @@ export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
