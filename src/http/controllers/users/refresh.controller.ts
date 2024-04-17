eaa605cb774b0eee44ed0f0ee50badda102bd360
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-16 23:46:10 -0300

    feat(refresh.controller): adding refreshToken base controller + route

---
+++ src/http/controllers/users/refresh.controller.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  // ? Check if the refreshToken exists in the request cookies
+  await request.jwtVerify({ onlyCookie: true })
+
+  const token = await reply.jwtSign({}, { sign: { sub: request.user.sub } })
+  const refreshToken = await reply.jwtSign(
+    {},
+    { sign: { sub: request.user.sub, expiresIn: '7d' } },
+  )
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/', // ? This enables the cookie to be accessible from all routes
+      secure: true, // ? This ensures that the cookie is only sent over HTTPS (secure)
+      sameSite: true, // ? This prevents the cookie from being sent in cross-site requests
+      httpOnly: true, // ? This prevents the cookie from being accessed via client/browser
+    })
+    .status(200)
+    .send({ token })
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,12 +1,16 @@
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './authenticate.controller'
 import { profile } from './profile.controller'
+import { refresh } from './refresh.controller'
 import { register } from './register.controller'
-import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Refresh token route
+  app.patch('/token/refresh', refresh)
 
   // Authenticated routes
   app.get('/me', { onRequest: [verifyJWT] }, profile)
