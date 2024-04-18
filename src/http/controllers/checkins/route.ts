97b29f794b603e43424814c75e0404190a569717
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-17 22:34:36 -0300

    refactor(admin-routes): passing new middleware to admin only routes

--- src/http/controllers/checkins/route.ts
+++ src/http/controllers/checkins/route.ts
@@ -1,4 +1,5 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 import { FastifyInstance } from 'fastify'
 import { create } from './create.controller'
 import { history } from './history.controller'
@@ -12,5 +13,9 @@ export async function checkinsRoutes(app: FastifyInstance) {
   app.get('/checkins/metrics', metrics)
 
   app.post('/gyms/:gymId/checkins', create)
-  app.patch('/checkins/:checkinId/validate', validate)
+  app.patch(
+    '/checkins/:checkinId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 }
